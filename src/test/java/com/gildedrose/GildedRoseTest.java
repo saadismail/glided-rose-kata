9572e18ca4483bcbaf72dbe5b0f34afe4e6e8537
Saad Ismail <saad3112@gmail.com>
2020-04-11 16:35:08 +0500

    add seperate tests for backstage

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -23,4 +23,31 @@ public class GildedRoseTest {
         assertEquals(2, items[0].sellIn);
     }
 
+    @Test
+    public void updateBackstageGreaterThan10DaysTest() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 15, 6) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(7, items[0].quality);
+        assertEquals(14, items[0].sellIn);
+    }
+
+    @Test
+    public void updateBackstageLessThan10DaysButGreaterThan5Test() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 8, 6) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(8, items[0].quality);
+        assertEquals(7, items[0].sellIn);
+    }
+
+    @Test
+    public void updateBackstageLessThan5DaysTest() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 3, 6) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(9, items[0].quality);
+        assertEquals(2, items[0].sellIn);
+    }
+
 }
