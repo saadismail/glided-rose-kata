9ba31be32c46937b02ee6a722c38e72d956c27c1
AmirQadir <K163641@nu.edu.pk>
2020-04-11 15:07:00 +0500

    [Tests] agedBrie and qualityBoundCheck

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -8,10 +8,29 @@ public class GildedRoseTest {
 
     @Test
     public void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        Item[] items = new Item[]{new Item("foo", 0, 0)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals("fixme", app.items[0].name);
     }
+
+    @Test
+    public void agedBrie() {
+        Item item = new Item("Aged Brie", 2, 0);
+        GildedRose gildedRose = new GildedRose(new Item[]{item});
+        gildedRose.updateQuality();
+        assertEquals(1, item.sellIn);
+        assertEquals(1, item.quality);
+    }
+
+    @Test
+    public void agedBrie_qualityCannotExceed50() {
+        Item item = new Item("Aged Brie", 2, 50);
+        GildedRose gildedRose = new GildedRose(new Item[]{item});
+        gildedRose.updateQuality();
+        assertEquals(1, item.sellIn);
+        assertEquals(50, item.quality);
+    }
+
 
 }
