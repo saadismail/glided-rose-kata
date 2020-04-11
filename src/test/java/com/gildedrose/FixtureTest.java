bc6dee10f55022ae0acf52d7cf46ade85360816c
Saad Ismail <saad3112@gmail.com>
2020-04-11 16:12:04 +0500

    add fixture test consider conured calculation

--- src/test/java/com/gildedrose/FixtureTest.java
+++ src/test/java/com/gildedrose/FixtureTest.java
@@ -9,7 +9,7 @@ import static org.junit.Assert.assertEquals;
 
 public class FixtureTest {
     @Test
-    public void foo() {
+    public void failingFixtureTest() {
         Item[] items = new Item[] {
                 new Item("+5 Dexterity Vest", 10, 20), //
                 new Item("Aged Brie", 2, 0), //
@@ -60,6 +60,64 @@ public class FixtureTest {
                 "Backstage passes to a TAFKAL80ETC concert, 9, 50\n" +
                 "Backstage passes to a TAFKAL80ETC concert, 4, 50\n" +
                 "Conjured Mana Cake, 2, 5" +
+                "\n\n";
+
+        String actualOutput = byteArrayOutputStream.toString();
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void passingFixtureTest() {
+        Item[] items = new Item[] {
+                new Item("+5 Dexterity Vest", 10, 20), //
+                new Item("Aged Brie", 2, 0), //
+                new Item("Elixir of the Mongoose", 5, 7), //
+                new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+                new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+                // this conjured item does not work properly yet
+                new Item("Conjured Mana Cake", 3, 6) };
+
+        GildedRose app = new GildedRose(items);
+
+        int days = 2;
+
+        PrintStream standardPrintStream = System.out;
+
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(byteArrayOutputStream));
+
+        app.printOutput(days);
+
+        System.out.flush();
+        System.setOut(standardPrintStream);
+
+        String expectedOutput = "OMGHAI!\n" +
+                "-------- day 0 --------\n" +
+                "name, sellIn, quality\n" +
+                "+5 Dexterity Vest, 10, 20\n" +
+                "Aged Brie, 2, 0\n" +
+                "Elixir of the Mongoose, 5, 7\n" +
+                "Sulfuras, Hand of Ragnaros, 0, 80\n" +
+                "Sulfuras, Hand of Ragnaros, -1, 80\n" +
+                "Backstage passes to a TAFKAL80ETC concert, 15, 20\n" +
+                "Backstage passes to a TAFKAL80ETC concert, 10, 49\n" +
+                "Backstage passes to a TAFKAL80ETC concert, 5, 49\n" +
+                "Conjured Mana Cake, 3, 6\n" +
+                "\n" +
+                "-------- day 1 --------\n" +
+                "name, sellIn, quality\n" +
+                "+5 Dexterity Vest, 9, 19\n" +
+                "Aged Brie, 1, 1\n" +
+                "Elixir of the Mongoose, 4, 6\n" +
+                "Sulfuras, Hand of Ragnaros, 0, 80\n" +
+                "Sulfuras, Hand of Ragnaros, -1, 80\n" +
+                "Backstage passes to a TAFKAL80ETC concert, 14, 21\n" +
+                "Backstage passes to a TAFKAL80ETC concert, 9, 50\n" +
+                "Backstage passes to a TAFKAL80ETC concert, 4, 50\n" +
+                "Conjured Mana Cake, 2, 4" +
                 "\n\n";
 
         String actualOutput = byteArrayOutputStream.toString();
