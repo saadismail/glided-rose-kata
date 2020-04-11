b329522557254a6afe81d1c80c035d575564321e
lamak <lamak.qaizar@careem.com>
2020-04-11 16:29:17 +0500

    Set quality to 0 for backstage

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -38,7 +38,6 @@ class GildedRose {
 
             items[i].sellIn = items[i].sellIn - 1;
 
-
             if (items[i].sellIn < 0) {
                 if (!items[i].name.equals("Aged Brie")) {
                     if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
@@ -46,7 +45,7 @@ class GildedRose {
                             items[i].quality = items[i].quality - 1;
                         }
                     } else {
-                        items[i].quality = items[i].quality - items[i].quality;
+                        items[i].quality = 0;
                     }
                 } else {
                     if (items[i].quality < 50) {
