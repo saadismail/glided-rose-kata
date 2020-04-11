a8d534f0c97e357752f37da9bdde74bc4b8a83ed
Saad Ismail <saad3112@gmail.com>
2020-04-11 16:18:49 +0500

    introduce boolean functions

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -7,34 +7,35 @@ class GildedRose {
         this.items = items;
     }
 
-    public void printOutput(int days) {
-        System.out.println("OMGHAI!");
-
-        for (int i = 0; i < days; i++) {
-            System.out.println("-------- day " + i + " --------");
-            System.out.println("name, sellIn, quality");
-            for (Item item : this.items) {
-                System.out.println(item);
-            }
-            System.out.println();
-            this.updateQuality();
-        }
-    }
-
     private boolean isEqual(Item item, String name) {
         return item.name.equals(name);
     }
 
-    public void updateQuality() {
+    private boolean isAgedBrie(Item item) {
+        return item.name.equals("Aged Brie");
+    }
 
+    private boolean isBackstage(Item item) {
+        return item.name.equals("Backstage passes to a TAFKAL80ETC concert");
+    }
+
+    private boolean isSulfuras(Item item) {
+        return item.name.equals("Sulfuras, Hand of Ragnaros");
+    }
+
+    private boolean isConjured(Item item) {
+        return item.name.equals("Conjured Mana Cake");
+    }
+
+    public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (isEqual(items[i], "Aged Brie")) {
+            if (isAgedBrie(items[i])) {
                 updateAgedbrieQuality(i);
-            } else if (isEqual(items[i], "Backstage passes to a TAFKAL80ETC concert")) {
+            } else if (isBackstage(items[i])) {
                 updateBackstageQuality(i);
-            } else if (isEqual(items[i], "Sulfuras, Hand of Ragnaros")) {
+            } else if (isSulfuras(items[i])) {
 //                TODO: quality can not be other than 80, check and throw exception
-            } else if (isEqual(items[i], "Conjured Mana Cake")) {
+            } else if (isConjured(items[i])) {
                 updateConjuredQuality(i);
             } else {
                 updateGeneralQuality(i);
@@ -45,8 +46,6 @@ class GildedRose {
             }
         }
     }
-
-
 
     private void updateGeneralQuality(int i) {
         items[i].quality--;
@@ -80,5 +79,19 @@ class GildedRose {
     private void updateConjuredQuality(int i) {
         items[i].quality -= 2;
         resetQualityToZeroIfNegative(i);
+    }
+
+    public void printOutput(int days) {
+        System.out.println("OMGHAI!");
+
+        for (int i = 0; i < days; i++) {
+            System.out.println("-------- day " + i + " --------");
+            System.out.println("name, sellIn, quality");
+            for (Item item : this.items) {
+                System.out.println(item);
+            }
+            System.out.println();
+            this.updateQuality();
+        }
     }
 }
