63db56d6fcdfc31303b70b5862b1886d92336ad0
lamak <lamak.qaizar@careem.com>
2020-04-11 16:35:58 +0500

    Merged nested if's with AND

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -12,6 +12,7 @@ class GildedRose {
             if (items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
                 continue;
             }
+
             if (isNotAgedBrieAndNotBackstage(items[i])) {
                 if (items[i].quality > 0) {
                     items[i].quality = items[i].quality - 1;
@@ -21,16 +22,12 @@ class GildedRose {
                     items[i].quality = items[i].quality + 1;
 
                     if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
+                        if (items[i].sellIn < 11 && items[i].quality < 50) {
+                            items[i].quality = items[i].quality + 1;
                         }
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
+                        if (items[i].sellIn < 6 && items[i].quality < 50) {
+                            items[i].quality = items[i].quality + 1;
                         }
                     }
                 }
@@ -38,18 +35,22 @@ class GildedRose {
 
             items[i].sellIn = items[i].sellIn - 1;
 
-            if (items[i].sellIn < 0) {
-                if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    items[i].quality = 0;
+            processNegativeSellIn(items[i]);
+        }
+    }
+
+    private void processNegativeSellIn(Item item) {
+        if (item.sellIn < 0) {
+            if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                item.quality = 0;
+            }
+            if (!item.name.equals("Aged Brie")) {
+                if (item.quality > 0) {
+                    item.quality = item.quality - 1;
                 }
-                if (!items[i].name.equals("Aged Brie")) {
-                        if (items[i].quality > 0) {
-                            items[i].quality = items[i].quality - 1;
-                        }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
+            } else {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
                 }
             }
         }
