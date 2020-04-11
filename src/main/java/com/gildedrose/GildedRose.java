565709ad435dc7cb43dffe0fc39ec8bd0a240cd1
lamak <lamak.qaizar@careem.com>
2020-04-11 16:39:34 +0500

    Extracted process quality for backstage into separate method

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -21,25 +21,29 @@ class GildedRose {
                 if (items[i].quality < 50) {
                     items[i].quality = items[i].quality + 1;
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11 && items[i].quality < 50) {
-                            items[i].quality = items[i].quality + 1;
-                        }
-
-                        if (items[i].sellIn < 6 && items[i].quality < 50) {
-                            items[i].quality = items[i].quality + 1;
-                        }
-                    }
+                    processQualityForBackstage(items[i]);
                 }
             }
 
             items[i].sellIn = items[i].sellIn - 1;
 
-            processNegativeSellIn(items[i]);
+            processQualityForNegativeSellIn(items[i]);
         }
     }
 
-    private void processNegativeSellIn(Item item) {
+    private void processQualityForBackstage(Item item) {
+        if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (item.sellIn < 11 && item.quality < 50) {
+                item.quality = item.quality + 1;
+            }
+
+            if (item.sellIn < 6 && item.quality < 50) {
+                item.quality = item.quality + 1;
+            }
+        }
+    }
+
+    private void processQualityForNegativeSellIn(Item item) {
         if (item.sellIn < 0) {
             if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 item.quality = 0;
