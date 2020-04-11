cc3cfe66795632f4baf5ce21cd8693db351abf14
lamak <lamak.qaizar@careem.com>
2020-04-11 16:31:06 +0500

    Moved up backstage pass set quality 0 to reduce nesting

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -39,13 +39,14 @@ class GildedRose {
             items[i].sellIn = items[i].sellIn - 1;
 
             if (items[i].sellIn < 0) {
+                if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    items[i].quality = 0;
+                }
                 if (!items[i].name.equals("Aged Brie")) {
                     if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                         if (items[i].quality > 0) {
                             items[i].quality = items[i].quality - 1;
                         }
-                    } else {
-                        items[i].quality = 0;
                     }
                 } else {
                     if (items[i].quality < 50) {
