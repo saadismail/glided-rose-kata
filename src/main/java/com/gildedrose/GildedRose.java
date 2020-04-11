73f7d589e8e83ecb70b5c84e9c3bed502b52599a
AmirQadir <K163641@nu.edu.pk>
2020-04-11 16:49:20 +0500

    [Refactored] separated case for backstage.

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -13,6 +13,20 @@ class GildedRose {
                 continue;
             }
 
+            if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (items[i].quality < 50) {
+                    items[i].quality = items[i].quality + 1;
+                    processQualityForBackstage(items[i]);
+                }
+
+                items[i].sellIn = items[i].sellIn - 1;
+
+                if (items[i].sellIn < 0) {
+                    items[i].quality = 0;
+                }
+                continue;
+            }
+
             if (isNotAgedBrieAndNotBackstage(items[i])) {
                 if (items[i].quality > 0) {
                     items[i].quality = items[i].quality - 1;
@@ -21,7 +35,6 @@ class GildedRose {
                 if (items[i].quality < 50) {
                     items[i].quality = items[i].quality + 1;
 
-                    processQualityForBackstage(items[i]);
                 }
             }
 
@@ -45,9 +58,7 @@ class GildedRose {
 
     private void processQualityForNegativeSellIn(Item item) {
         if (item.sellIn < 0) {
-            if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                item.quality = 0;
-            }
+
             if (!item.name.equals("Aged Brie")) {
                 if (item.quality > 0) {
                     item.quality = item.quality - 1;
@@ -61,7 +72,6 @@ class GildedRose {
     }
 
     private boolean isNotAgedBrieAndNotBackstage(Item item) {
-        return !item.name.equals("Aged Brie")
-                && !item.name.equals("Backstage passes to a TAFKAL80ETC concert");
+        return !item.name.equals("Aged Brie");
     }
 }
