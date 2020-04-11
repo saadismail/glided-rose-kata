9b558071c2c5646f511d8bd322bcce90376edc36
lamak <lamak.qaizar@careem.com>
2020-04-11 16:16:45 +0500

    Extracted NotBrie and NorBackstage check into separate method

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -9,8 +9,7 @@ class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (isNotAgedBrieAndNotBackstage(items[i])) {
                 if (items[i].quality > 0) {
                     if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
                         items[i].quality = items[i].quality - 1;
@@ -59,5 +58,10 @@ class GildedRose {
                 }
             }
         }
+    }
+
+    private boolean isNotAgedBrieAndNotBackstage(Item item) {
+        return !item.name.equals("Aged Brie")
+                && !item.name.equals("Backstage passes to a TAFKAL80ETC concert");
     }
 }
