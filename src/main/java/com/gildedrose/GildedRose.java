dd203b66394190ac01bf096d87216c04c9632208
lamak <lamak.qaizar@careem.com>
2020-04-11 16:21:48 +0500

    Moved Sulfuras case to top of method. Don't need to subtract sellin or quality for it

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -9,11 +9,12 @@ class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
+            if (items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                continue;
+            }
             if (isNotAgedBrieAndNotBackstage(items[i])) {
                 if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
+                    items[i].quality = items[i].quality - 1;
                 }
             } else {
                 if (items[i].quality < 50) {
@@ -35,9 +36,8 @@ class GildedRose {
                 }
             }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
+            items[i].sellIn = items[i].sellIn - 1;
+
 
             if (items[i].sellIn < 0) {
                 if (!items[i].name.equals("Aged Brie")) {
